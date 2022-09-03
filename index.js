c3612320b8623274e8c716fd2ff250fd23d88391
Júlio César Agripino de Oliveira <juliocesar014@live.com>
2022-09-03 12:04:31 -0300

    front do projeto

---
+++ index.js
@@ -0,0 +1,29 @@
+const openBtn = document.querySelector('button');
+const closeBtn = document.querySelector('#close');
+const modal = document.querySelector('.modal');
+
+openBtn.addEventListener('click', openModal);
+
+function openModal() {
+    modal.style.display = 'grid';
+}
+
+closeBtn.addEventListener('click', closeModal);
+
+function closeModal() {
+    modal.classList.add('close-modal-animation');
+    setTimeout(() => {
+        modal.style.display = 'none';
+        window.location.reload();
+    }, 1000)
+}
+
+
+modal.addEventListener('click', function (e) {
+    if (e.target.classList.contains('modal')) {
+        closeModal();
+    }
+})
+
+
+// Mensagem de cadastro.
---
+++ readme.md
@@ -0,0 +1,21 @@
+## Newsletter 
+### Criação de uma Newsletter que informará diariamente notícias do mercado de criptomoedas!
+
+### 1- Criação do HTML;
+### 2- Criação do CSS;
+### 3- Criação do JS;
+
+
+
+
+
+
+
+### Linguagens usadas:
+#### -> Html - CSS - JavaScript - Python(Flask)
+
+### Tarefas pendentes:
+### -> Mensagem final de inscrito,
+### -> Favicon, 
+### -> Receber e-mails com Flask-Mail,
+### -> Backend com Python (Flask),
